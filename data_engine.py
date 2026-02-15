58b2dca8d13a730895dbf4d418e5942d68006ef9
unidadconsciente <enriqueasv93@gmail.com>
2026-02-15 16:57:42 -0600

    Update data_engine.py

--- data_engine.py
+++ data_engine.py
@@ -3,13 +3,16 @@ from google.oauth2.service_account import Credentials
 import pandas as pd
 import streamlit as st
 
-def get_google_data(sheet_id):
+def get_google_data():
     scope = ["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive"]
     creds = Credentials.from_service_account_info(st.secrets["gcp_service_account"], scopes=scope)
     client = gspread.authorize(creds)
-    doc = client.open_by_key(sheet_id)
     
-    # Retornamos los DataFrames listos
-    df_cont = pd.DataFrame(doc.worksheet("contenido").get_all_records())
+    doc = client.open_by_key("1_zDRbDQ3KXl8aiFNkEuOsKLIlKwRc8bD4fyVHguMKI4")
+    
+    # Cargamos las 3 fuentes de verdad
+    df_cont = pd.DataFrame(doc.worksheet("Contenido").get_all_records())
     df_conf = pd.DataFrame(doc.worksheet("Config_Archivos").get_all_records())
-    return df_cont, df_conf
+    df_avatar = pd.DataFrame(doc.worksheet("Avatar_nuevo").get_all_records())
+    
+    return df_cont, df_conf, df_avatar
