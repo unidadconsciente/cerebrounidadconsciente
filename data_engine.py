a62a779b2b5fa1ff05846c2993b47060d2254697
unidadconsciente <enriqueasv93@gmail.com>
2026-02-17 17:18:11 -0600

    Update data_engine.py

--- data_engine.py
+++ data_engine.py
@@ -1,25 +1,22 @@
-import streamlit as st
-import pandas as pd
 import gspread
-
+import pandas as pd
+import streamlit as st
 from google.oauth2.service_account import Credentials
+
 def get_google_data():
-
     scope = ["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive"]
-
     creds = Credentials.from_service_account_info(st.secrets["gcp_service_account"], scopes=scope)
     client = gspread.authorize(creds)
-
+    
     sheet = client.open_by_key("1_zDRbDQ3KXl8aiFNkEuOsKLIlKwRc8bD4fyVHguMKI4")
-
+    
+    # Solo cargamos las 3 pestañas esenciales
     avatar = pd.DataFrame(sheet.worksheet("Avatar_nuevo").get_all_records())
     contenido = pd.DataFrame(sheet.worksheet("Contenido").get_all_records())
     configuracion = pd.DataFrame(sheet.worksheet("Config_Archivos").get_all_records())
-    opciones = pd.DataFrame(sheet.worksheet("Config_Opciones").get_all_records())
 
-    avatar.columns = [c.strip() for c in avatar.columns]
-    contenido.columns = [c.strip() for c in contenido.columns]
-    configuracion.columns = [c.strip() for c in configuracion.columns]
-    opciones.columns = [c.strip() for c in opciones.columns]
+    # Limpieza de columnas
+    for df in [avatar, contenido, configuracion]:
+        df.columns = [str(c).strip() for c in df.columns]
 
-    return avatar, contenido, configuracion, opciones
+    return avatar, contenido, configuracion
